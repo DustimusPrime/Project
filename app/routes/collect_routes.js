4b16d886a14fa0e268e9ad6314de4ea3f170c322
Dusty Stokes <dustys@accretivetg.com>
2017-05-23 10:59:01 -0700

    imdb working

--- app/routes/collect_routes.js
+++ app/routes/collect_routes.js
@@ -1,12 +1,19 @@
 module.exports = function(app, db){
 
 const imdb = require('imdb-api');
-	
+
 	app.post('/collect', (req, res) => {
-		const newMovieID = { text: req.body.id };
+		const newMovieId = { text: req.body.movieId };
 
 		let movie;
-		imdb.getById('newMovieID', {apiKey: '1fdc329a'}).then(console.log);
+		imdb.getById(newMovieId.text, {apiKey: '1fdc329a'}).then(console.log);
 
+    db.collection('library').insert(movie, (err, result) => {
+      if (err) {
+        res.send({'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
 	});
 };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //server.js
 
-const express 		= require('express');
+const express 		  = require('express');
 const MongoClient 	= require('mongodb').MongoClient;
-const bodyParser 	= require('body-parser');
-const db			= require('./config/db');
+const bodyParser 	  = require('body-parser');
+const db			      = require('./config/db');
 
 const app = express();
 
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended:true }));
 MongoClient.connect(db.url, (err, database) => {
 	if (err) return console.log(err)
 	require('./app/routes')(app, database);
-	
+
 	app.listen(port, () => {
-		console.log('We are live on' + port);
+		console.log('We are live on ' + port);
 	});
 })
